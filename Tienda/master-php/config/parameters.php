2ff131055ce04c0706d865b3cb3bbcd2d041d277
Javier Villarejo Gayol <44836850+Restivejuvi@users.noreply.github.com>
2022-01-25 21:18:09 +0100

    Inicio
    Primeros pasos

---
+++ Tienda/master-php/config/parameters.php
@@ -0,0 +1,7 @@
+<?php
+define("base_url", "http://localhost/Taller/Tienda/master-php/"); //cambiar por el mio
+// Raul
+// Ivan
+define("controller_default", "productoController");
+define("action_default", "index");
+
