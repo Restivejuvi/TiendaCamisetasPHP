4620de2c52aff2b38d949b827ef24edac9bd9505
Javier Villarejo Gayol <44836850+Restivejuvi@users.noreply.github.com>
2022-02-20 20:26:47 +0100

    ajuste

--- Tienda/master-php/config/parameters.php
+++ Tienda/master-php/config/parameters.php
@@ -1,7 +1,5 @@
 <?php
-define("base_url", "http://localhost/Taller/Tienda/master-php/"); //cambiar por el mio
-// Raul define("base_url", "http://localhost/tallerPHP/master-php/");
-// Ivan
+define("base_url", "http://localhost/Taller/Tienda/master-php/");
 define("controller_default", "productoController");
 define("action_default", "index");
 
